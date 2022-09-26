b96442ff591aaee10f0e166a8a8eb0ccc03721f1
yuiceline <113922020+yuiceline@users.noreply.github.com>
2022-09-26 14:33:18 +0900

    Updated time section

--- src/app.js
+++ src/app.js
@@ -1,4 +1,20 @@
 
+function formatDate(timestamp) {
+ //calculate the date
+ let date = new Date(timestamp);
+ let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+ let minutes = date.getMinutes();
+ if (minutes < 10) {
+    minutes = `0${minutes}`;
+ }
+ let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+ let day = days[date.getDay()];
+ return `${day} ${hours}:${minutes}`
+}
+
 function displayTemperature(response) {
 console.log(response.data);
  
@@ -16,6 +32,10 @@ currentCondition.innerHTML = response.data.weather[0].description;
 
  let currentWind = document.querySelector("#current-wind");
  currentWind.innerHTML = Math.round(response.data.wind.speed);
+
+ let date = document.querySelector("#date");
+ date.innerHTML = formatDate(response.data.dt * 1000);
+
 }
 
 let apiKey = "690c1586235f5c036bd74a5466b0f1f4";
