dd9efe5f8bf45c768fff289a00af3e7d6bcdd2c0
yuiceline <113922020+yuiceline@users.noreply.github.com>
2022-09-28 13:56:11 +0900

    Added search engine

--- src/app.js
+++ src/app.js
@@ -1,47 +1,67 @@
-
 function formatDate(timestamp) {
- let date = new Date(timestamp);
- let hours = date.getHours();
+  let date = new Date(timestamp);
+  let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
- let minutes = date.getMinutes();
- if (minutes < 10) {
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
     minutes = `0${minutes}`;
- }
- let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
- let day = days[date.getDay()];
- return `${day} ${hours}:${minutes}`
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
-let currentCity = document.querySelector("#current-city");
-currentCity.innerHTML = response.data.name;
+  let currentCity = document.querySelector("#current-city");
+  currentCity.innerHTML = response.data.name;
 
- let currentTemperature = document.querySelector("#current-temp");
- currentTemperature.innerHTML = Math.round(response.data.main.temp);
+  let currentTemperature = document.querySelector("#current-temp");
+  currentTemperature.innerHTML = Math.round(response.data.main.temp);
 
-let currentCondition = document.querySelector("#weather-description");
-currentCondition.innerHTML = response.data.weather[0].description;
+  let currentCondition = document.querySelector("#weather-description");
+  currentCondition.innerHTML = response.data.weather[0].description;
 
- let currentHumidity = document.querySelector("#current-humidity");
- currentHumidity.innerHTML = response.data.main.humidity;
+  let currentHumidity = document.querySelector("#current-humidity");
+  currentHumidity.innerHTML = response.data.main.humidity;
 
- let currentWind = document.querySelector("#current-wind");
- currentWind.innerHTML = Math.round(response.data.wind.speed);
+  let currentWind = document.querySelector("#current-wind");
+  currentWind.innerHTML = Math.round(response.data.wind.speed);
 
- let date = document.querySelector("#date");
- date.innerHTML = formatDate(response.data.dt * 1000);
+  let date = document.querySelector("#date");
+  date.innerHTML = formatDate(response.data.dt * 1000);
 
- let weatherIcon = document.querySelector("#icon");
- weatherIcon.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  let weatherIcon = document.querySelector("#icon");
+  weatherIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 
- weatherIcon.setAttribute("alt", response.data.weather[0].description);
+  weatherIcon.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "690c1586235f5c036bd74a5466b0f1f4";
-let cityName = "New York"
-let apiUrl =
-  `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
-  
+function search(city) {
+  let apiKey = "690c1586235f5c036bd74a5466b0f1f4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  search(cityInput.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
