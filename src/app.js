b2720f9c8172639820ba75102582f13811b8f17c
yuiceline <113922020+yuiceline@users.noreply.github.com>
2022-10-04 18:45:28 +0900

    Updated to display daily forecast data from API

--- src/app.js
+++ src/app.js
@@ -21,26 +21,36 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+ let date = new Date(timestamp * 1000);
+ let day = date.getDay();
+ let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+ return days[day];
+}
+
 function displayForecast(response) {
-    console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
+
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
     forecastHTML =
       forecastHTML +
       ` 
               <div class="col-2">
                 <div class="weather-forecast-date">
-                  ${day}
+                  ${formatDay(forecastDay.dt)}
                 </div>
-                <img src="http://openweathermap.org/img/wn/10n@2x.png" alt="" width="40px">
+                <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="40px">
                 <div class="weather-forecast-temp">
-                  <span class="weather-forecast-max">18°</span> <span class="weather-forecast-min">12°</span>
+                  <span class="weather-forecast-max">${Math.round(forecastDay.temp.max)}°</span> <span class="weather-forecast-min">${Math.round(forecastDay.temp.min)}°</span>
                 </div>
               </div>
             
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -80,7 +90,6 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 
- 
   weatherIcon.setAttribute("alt", response.data.weather[0].description);
 
   getForecast(response.data.coord);
