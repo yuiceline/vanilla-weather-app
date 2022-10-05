ed9e858140db743f637a82f56bfaf9c638ceae11
yuiceline <113922020+yuiceline@users.noreply.github.com>
2022-10-05 23:44:31 +0900

    Added some stylings

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ function displayForecast(response) {
                 <div class="weather-forecast-date">
                   ${formatDay(forecastDay.dt)}
                 </div>
-                <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="40px">
+                <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="40px" class="forecast-icon">
                 <div class="weather-forecast-temp">
                   <span class="weather-forecast-max">${Math.round(forecastDay.temp.max)}°</span> <span class="weather-forecast-min">${Math.round(forecastDay.temp.min)}°</span>
                 </div>
